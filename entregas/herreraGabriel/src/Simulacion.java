0562b50d26ac38bbd476ef07fefa5627520f59b6
Gabriel19-87 <gabriel.herrera@alumnos.uneatlantico.es>
2026-09-20 13:52:39 +0200

    feat: Integrar eventos del reto extendido y control temporal a partir del minuto 20 en Simulacion.java

--- entregas/herreraGabriel/src/Simulacion.java
+++ entregas/herreraGabriel/src/Simulacion.java
@@ -2,31 +2,60 @@ public class Simulacion {
     private int minutosTotales;
     private Fila fila;
     private CajaRegistradora caja;
+    private int contadorId;
 
     public Simulacion(int minutosTotales) {
         this.minutosTotales = minutosTotales;
         this.fila = new Fila();
         this.caja = new CajaRegistradora();
+        this.contadorId = 1;
     }
 
     public void ejecutar() {
         for (int minuto = 1; minuto <= this.minutosTotales; minuto++) {
             
+            this.fila.actualizarTiempos();
+
             if (Math.random() < 0.6) {
-                this.fila.formarPersona();
+                Persona nuevaPersona;
+                if (Math.random() < 0.2) {
+                    nuevaPersona = new ClientePreferente(String.valueOf(contadorId++), "Prioridad Legal");
+                } else {
+                    nuevaPersona = new ClienteNormal(String.valueOf(contadorId++));
+                }
+                this.fila.formarPersona(nuevaPersona);
             }
 
             if (Math.random() < 0.4) {
-                if (!this.fila.estaVacia()) {
-                    this.fila.salirPersona();
+                Persona atendida = this.fila.salirPersona();
+                if (atendida != null) {
                     this.caja.atenderPersona();
                 }
+            }
+
+            if (minuto >= 20) {
+                if (minuto % 5 == 0) {
+                    this.fila.revisarAburridos();
+                }
+
+                if (Math.random() < 0.05) {
+                    Persona colado = new ClienteNormal(contadorId++ + "Colado");
+                    this.fila.colarConocido(colado);
+                }
+
+                if (Math.random() < 0.05) {
+                    this.fila.entregarComprasAConocido();
+                }
+            }
+
+            if (minuto % 15 == 0 && this.fila.getLongitud() > 25) {
+                System.out.println("[Minuto " + minuto + "] : Pasen por esta caja en orden de fila.");
             }
         }
     }
 
     public void mostrarResultados() {
-        System.out.println("=== Resultados CCCF tras " + this.minutosTotales + " minutos ===");
+        System.out.println("Resultados CCCF tras " + this.minutosTotales + " minutos ");
         System.out.println("Personas que fueron atendidas: " + this.caja.getTotalAtendidos());
         System.out.println("Personas que quedaron en la fila: " + this.fila.getLongitud());
     }
