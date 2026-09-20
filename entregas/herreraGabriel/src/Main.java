d2b8f0b2c8c4ccb99b67f70ef6a49063beb26738
Gabriel19-87 <gabriel.herrera@alumnos.uneatlantico.es>
2026-09-20 13:36:43 +0200

    feat: Configurar Main limpio

---
+++ entregas/herreraGabriel/src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        Simulacion cccf = new Simulacion(240); 
+        
+        cccf.ejecutar();
+        cccf.mostrarResultados();
+    }
+}
